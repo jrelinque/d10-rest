be78661ca949de137ef61a3ad08c65edc3539676
Jesús Relinque <jrelinque@emergya.com>
2024-02-19 13:31:32 +0100

    Deleting files

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -865,3 +865,46 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+$host = "db";
+$port = 3306;
+$driver = "mysql";
+
+$databases['default']['default']['database'] = "db";
+$databases['default']['default']['username'] = "db";
+$databases['default']['default']['password'] = "db";
+$databases['default']['default']['host'] = $host;
+$databases['default']['default']['port'] = $port;
+$databases['default']['default']['driver'] = $driver;
+
+$settings['hash_salt'] = 'cf07f43949436dcc7bfad7ad6f9bdde035fcd3457eca15660dc659a2173c96a3';
+
+// This will prevent Drupal from setting read-only permissions on sites/default.
+$settings['skip_permissions_hardening'] = TRUE;
+
+// This will ensure the site can only be accessed through the intended host
+// names. Additional host patterns can be added for custom configurations.
+$settings['trusted_host_patterns'] = ['.*'];
+
+// Don't use Symfony's APCLoader. ddev includes APCu; Composer's APCu loader has
+// better performance.
+$settings['class_loader_auto_detect'] = FALSE;
+
+// Set $settings['config_sync_directory'] if not set in settings.php.
+if (empty($settings['config_sync_directory'])) {
+  $settings['config_sync_directory'] = 'sites/default/files/sync';
+}
+
+// Override drupal/symfony_mailer default config to use Mailpit
+$config['symfony_mailer.mailer_transport.sendmail']['plugin'] = 'smtp';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['user']='';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['pass']='';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['host']='localhost';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['port']='1025';
+
+// Enable verbose logging for errors.
+// https://www.drupal.org/forum/support/post-installation/2018-07-18/enable-drupal-8-backend-errorlogdebugging-mode
+$config['system.logging']['error_level'] = 'verbose';
+$settings['config_sync_directory'] = 'sites/default/sync';
+
+
